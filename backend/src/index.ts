e52eaeb36bb23e36ab9cb2dc5ba983b3a2e5ccd6
Ashwani Jha <ash.6991.aj@gmail.com>
2024-02-13 23:12:50 +0530

    created add hotel API

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,8 +4,16 @@ import 'dotenv/config';
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
+import myHotelRoutes from './routes/myHotels';
 import cookieparser from 'cookie-parser';
 import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLDNRY_NAME,
+    api_key: process.env.CLDNRY_KEY,
+    api_secret: process.env.CLDNRY_SECRT
+})
 
 mongoose.connect(process.env.MONGO_DB_CONN_STR as string);
 
@@ -24,6 +32,7 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/my-hotels",myHotelRoutes);
 
 // Create Server
 app.listen(5500, () => {
---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const AddHotel = () => {
+  return (
+    // <ManageHotelForm />
+    <></>
+  )
+}
+
+export default AddHotel;
